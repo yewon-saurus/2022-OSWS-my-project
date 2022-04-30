1cb7174138c9d257cf3276baaf9f93ecd5d4c372
yewon0804 <ehdnjs0804@gmail.com>
2022-04-30 18:38:24 +0900

    User class 생성 및 D DB와의 B와 연연동

---
+++ src/main/java/user/User.java
@@ -0,0 +1,19 @@
+package user;
+
+public class User {
+	private String userEmail;
+	private String userPassword;
+	
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String  userPassword) {
+		this.userPassword = userPassword;
+	}
+}
---
+++ src/main/webapp/myHomePage/login.jsp
@@ -0,0 +1,18 @@
+<%@page import="org.apache.tomcat.jni.User"%>
+<%@page import="java.io.*" %>
+
+<%@ page import="user.UserDAO" %>
+<%@ page import="java.io.PrintWriter" %>
+
+<% request.setCharacterEncoding("UTF-8"); %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+</body>
+</html>
