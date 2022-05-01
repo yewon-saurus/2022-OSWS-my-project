c7e1e7c674b16f483fc7c3f0083d4c71406d3a32
yewon0804 <ehdnjs0804@gmail.com>
2022-05-02 02:32:10 +0900

    DB의 charset을 UTF-8로 설정

--- src/main/webapp/myHomePage/myHomePage.sql
+++ src/main/webapp/myHomePage/myHomePage.sql
@@ -5,6 +5,23 @@ CREATE TABLE `myhomepage`.`user` (
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
+  
+ALTER TABLE `myhomepage`.`user` 
+CHARACTER SET = utf8 , COLLATE = utf8_bin ;
+
 
 SELECT * FROM myhomepage.user;
 
+CREATE TABLE `myhomepage`.`board` (
+  `BoardID` INT NOT NULL,
+  `BoardTitle` VARCHAR(45) NULL,
+  `userID` VARCHAR(45) NULL,
+  `BoardDate` VARCHAR(45) NULL,
+  `BoardContent` TEXT NULL,
+  PRIMARY KEY (`BoardID`),
+  UNIQUE INDEX `BoardID_UNIQUE` (`BoardID` ASC) VISIBLE);
+
+ SELECT * FROM myhomepage.board;
+ 
+ALTER TABLE `myhomepage`.`board` 
+CHARACTER SET = utf8 , COLLATE = utf8_bin ;
